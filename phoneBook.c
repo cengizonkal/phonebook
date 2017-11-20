2e12a736c07df47a407bb611b0c4cfd01bd425dc
cengiz <onkal.cengiz@gmail.com>
2017-11-21 00:13:52 +0200

    ignore

--- phoneBook.c
+++ phoneBook.c
@@ -6,8 +6,9 @@ long int searchPhoneBook(FILE *fp, char *query) {
     struct phoneEntry entry;
     while (fread(&entry, sizeof(struct phoneEntry), 1, fp) == 1) {
 
-        if (strcmp(entry.firstName, query)) {
-
+        if (strcmp(entry.firstName, query) == 0) {
+            fseek(fp, -sizeof(struct phoneEntry), SEEK_CUR);
+            return ftell(fp);
         }
     }
 
